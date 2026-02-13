b8dd4c041de3dc7202dc89a60f0160caab94b3b5
Faizan-26 <f223858@cfd.nu.edu.pk>
2026-02-13 15:38:06 +0500

    initial commit

---
+++ run.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Starting RealEditor...
+java -cp "bin;resource\ADAT-Lemmatization.v1.20180101.jar;resource\ADAT-Racineur.v1.20180101.jar;resource\ADAT-Stemmer.v1.20180101.jar;resource\mariadb-java-client-3.4.1.jar;resource\AlKhalilDiacritizer.jar;resource\AlKhalilMorphoSys2.jar;resource\Pos_tagger.jar;resource\log4j-api-2.20.0.jar;resource\log4j-core-2.20.0.jar" Driver
+pause
